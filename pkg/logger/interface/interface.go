003776b3c06164bb9ffb6404c218e4169e38d95b
TJ Holowaychuk <tj@vision-media.ca>
2014-08-29 18:26:46 -0700

    add docs to logger_interface

--- pkg/logger/interface/interface.go
+++ pkg/logger/interface/interface.go
@@ -1,10 +1,21 @@
 package logger_interface
 
 type Logger interface {
+	// Skip a commit.
 	Skip(arg, commit string)
+
+	// Start a commit.
 	Start(arg, commit string)
+
+	// Successful commit.
 	Success(arg, commit string)
+
+	// Error executing a commit.
 	Error(err error, arg, commit string)
-	Log(ling string)
+
+	// Log line (comment).
+	Log(line string)
+
+	// End of provision.
 	End()
 }
